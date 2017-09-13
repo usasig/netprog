a7bdad2056fba40c42b4d968f8d4d35c3ad4cc1f
sasig <sasig@cisco.com>
2017-09-12 22:31:40 -0400

    modified according to my system

--- Module 5 Python Files/telnetRouter.py
+++ Module 5 Python Files/telnetRouter.py
@@ -1,7 +1,7 @@
 import getpass
 import telnetlib
 
-HOST = "192.168.1.77"
+HOST = input("Enter HOS/IP: ")
 user = input("Enter your username: ")
 password = getpass.getpass()
 
@@ -14,12 +14,11 @@ if password:
     tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"enable\n")
-tn.write(b"cisco\n")
-tn.write(b"conf t\n")
-tn.write(b"host HQ-ROUTER\n")
-tn.write(b"end\n")
-tn.write(b"copy run star\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
 tn.write(b"\n")
+tn.write(b"show ver\n")
 tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
